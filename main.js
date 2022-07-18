49bc5528f9da85a63f34277bb74046cbe383251c
rambabu-1929 <106583393+rambabu-1929@users.noreply.github.com>
2022-07-18 16:35:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function diceRoll () {
+    var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+if (randomNumber1 == 1) {
+    document.querySelector('.dice-1').setAttribute('src', './images/dice1.png');
+} else if (randomNumber1 == 2) {
+    document.querySelector('.dice-1').setAttribute('src', './images/dice2.png');
+} else if (randomNumber1 == 3) {
+    document.querySelector('.dice-1').setAttribute('src', './images/dice3.png');
+} else if (randomNumber1 == 4) {
+    document.querySelector('.dice-1').setAttribute('src', './images/dice4.png');
+} else if (randomNumber1 == 5) {
+    document.querySelector('.dice-1').setAttribute('src', './images/dice5.png');
+} else if (randomNumber1 == 6) {
+    document.querySelector('.dice-1').setAttribute('src', './images/dice6.png');
+}
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+if (randomNumber2 == 1) {
+    document.querySelector('.dice-2').setAttribute('src', './images/dice1.png');
+} else if (randomNumber2 == 2) {
+    document.querySelector('.dice-2').setAttribute('src', './images/dice2.png');
+} else if (randomNumber2 == 3) {
+    document.querySelector('.dice-2').setAttribute('src', './images/dice3.png');
+} else if (randomNumber2 == 4) {
+    document.querySelector('.dice-2').setAttribute('src', './images/dice4.png');
+} else if (randomNumber2 == 5) {
+    document.querySelector('.dice-2').setAttribute('src', './images/dice5.png');
+} else if (randomNumber2 == 6) {
+    document.querySelector('.dice-2').setAttribute('src', './images/dice6.png');
+}
+
+if (randomNumber1>randomNumber2) {
+    document.querySelector('h2').innerHTML = '🏆 Player 1 wines ';
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector('h2').innerHTML = 'Player 2 wines 🏆';
+} else if (randomNumber1 = randomNumber2) {
+    document.querySelector('h2').innerHTML = 'Match 🏆 Draw';
+}
+
+}
+
